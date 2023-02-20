2915ca3ec9dbc300af203b7cc5068937953df8a6
John Klink <john.r.klink92@gmail.com>
2023-02-20 14:00:09 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+const style = require('./src/template');
+
+function createTeam() {
+    inquirer
+      .prompt([
+        console.log('\nWelcome to the team builder'),
+        {
+          type: 'list',
+          name: 'memberChoice',
+          message: 'Which type of team member would you like to add?',
+          choices: [
+            'Manager',
+            'Engineer',
+            'Intern',
+            "I don't want to add any more team members",
+          ],
+        },
+      ])
+      .then((userChoice) => {
+        switch (userChoice.memberChoice) {
+        case 'Manager':
+            addManager();
+        case 'Engineer':
+            addEngineer();
+            break;
+        case 'Intern':
+            addIntern();
+            break;
+        default:
+            buildTeam();
+        }
+      });
+  }
+  console.log(createTeam)
